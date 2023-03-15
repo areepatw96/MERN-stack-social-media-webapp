4e4e0ebd2555fac4de3af1c6f3aee575b7225306
areepatw96 <arefapatw96@gmail.com>
2023-03-15 05:46:37 -0500

    auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,19 +3,20 @@ import jwt from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
     try {
 
-        let token = req.header("Authorization");
+        let token = req.header("Authorization"); 
         if (!token){
-            return res.status(403).send("Access Denied");
+            return res.status(403).send("Access Denied"); 
         } 
 
         if (token.startsWith("Bearer ")){
             token = token.slice(7, token.length).trimLeft();
         }
 
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(token, process.env.JWT_SECRET); 
         req.user = verified;
         next(); // used to move to the next middleware
 
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
+};
